13d19b0b520e941e0a8f6f7f6d755c12a021ebed
Tati Ramos <tatiramos@ibm.com>
2023-03-29 14:39:21 -0300

    ✨ Configuracao de CORS

--- src/main/java/com/tati/inventory/controller/CategoryController.java
+++ src/main/java/com/tati/inventory/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.tati.inventory.service.ICategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
